a1383554656cf1613ea7ee320fa218179460cf04
Ralph Hempel <rhempel@hempeldesigngroup.com>
2024-01-10 14:10:30 -0500

    Generalize register names

--- serial9.cpp
+++ serial9.cpp
@@ -12,50 +12,61 @@
      be completely finished before changing the state of the 9th bit.
 
   2. There is no need for buffereing the data from the host - we only read
-     a new character from the host when the 
+     a new character from the host when the
 */
 #include "Arduino.h"
 #include "serial9.h"
 
+#if !defined(HAVE_CDCSERIAL)
+  #error This project requires an Arduino with USB-CDC capabilities
+#endif
+
+#if !defined(ARDUINO_AVR_LEONARDO)
+  #error This project requires an AVR_LEONARDO compatible board (Pro Micro)
+#endif
+
 // Move this to a private H file
 #if defined(__AVR_ATmega32U4__)
-  #define TXC0 TXC1
-  #define RXC0 RXC1
-  #define RXEN0 RXEN1
-  #define TXEN0 TXEN1
-  #define RXCIE0 RXCIE1
-  #define UDRIE0 UDRIE1
-  #define U2X0 U2X1
-  #define UPE0 UPE1
-  #define UDRE0 UDRE1
-  #define UCSZ00 UCSZ10
-  #define UCSZ01 UCSZ11
-  #define UCSZ02 UCSZ12
-  #define TXCIE0 TXCIE1
-  #define TXB80 TXB81
-  #define RXB80 RXB81
+  #define TXC TXC1
+  #define RXC RXC1
+  #define RXEN RXEN1
+  #define TXEN TXEN1
+  #define RXCIE RXCIE1
+  #define UDRIE UDRIE1
+  #define U2X U2X1
+  #define UPE UPE1
+  #define UDRE UDRE1
+  #define UCSZ0 UCSZ10
+  #define UCSZ1 UCSZ11
+  #define UCSZ2 UCSZ12
+  #define TXCIE TXCIE1
+  #define TXB8 TXB81
+  #define RXB8 RXB81
+
+  #define UBRRH UBRR1H;
+  #define UBRRL UBRR1L;
+
+  #define UCSRA UCSR1A;
+  #define UCSRB UCSR1B;
+  #define UCSRC UCSR1C;
+  #define UDR UDR1;
+
+  #define RE_ (2)
+  #define DE  (3)
 #else
   #error This library currently only works with ATmega32U4
 #endif
 
-#define RE_ (2)
-#define DE  (3)
-
 Serial9::Serial9()
 {
-#if defined(UBRR1H)
-    _ubrrh = &UBRR1H;
-    _ubrrl = &UBRR1L;
+  _ubrrh = &UBRRH;
+  _ubrrl = &UBRRL;
+  _ucsra = &UCSRA;
+  _ucsrb = &UCSRB;
+  _ucsrc = &UCSRC;
+  _udr = &UDR;
 
-    _ucsra = &UCSR1A;
-    _ucsrb = &UCSR1B;
-    _ucsrc = &UCSR1C;
-    _udr = &UDR1;
-
-    rx_state = SERIAL9_STATE_IDLE;
-#else
-  #error Currently only Seria1 with ATmega32U4 is supported
-#endif
+  rx_state = SERIAL9_STATE_IDLE;
 }
 
 Serial9::~Serial9() {}
@@ -63,7 +74,7 @@ Serial9::~Serial9() {}
 void Serial9::begin(uint32_t baud)
 {
   uint16_t baud_setting = (F_CPU / 4 / baud - 1) / 2;
-  *_ucsra = 1 << U2X0;
+  *_ucsra = 1 << U2X;
 
   // hardcoded exception for 57600 for compatibility with the bootloader
   // shipped with the Duemilanove and previous boards and the firmware
@@ -82,11 +93,11 @@ void Serial9::begin(uint32_t baud)
 
   // disable interrupts, set 9bit mode, enable rx/tx
   //  This completely overwrites any previous UCSRB bits
-  *_ucsrb = (1 << UCSZ02) | (1 << TXEN0) | (1 << RXEN0) | // 9bits, enabled
-            (0 << UDRIE0) | (0 << TXCIE0) | (0 << RXCIE0); // ints off.
+  *_ucsrb = (1 << UCSZ2) | (1 << TXEN) | (1 << RXEN) | // 9bits, enabled
+            (0 << UDRIE) | (0 << TXCIE) | (0 << RXCIE); // ints off.
 
   // Enable 9bit size in UCSRC.  Leave the other bits alone (Parity, etc)
-  *_ucsrc |= (1 << UCSZ00) | (1 << UCSZ01);
+  *_ucsrc |= (1 << UCSZ0) | (1 << UCSZ1);
 
   _written = false;
 
@@ -100,8 +111,8 @@ void Serial9::begin(uint32_t baud)
 void Serial9::end()
 {
   // wait for transmission of outgoing data
-  *_ucsrb &= ~((1 << TXEN0) | (1 << RXEN0) | // 9bits, enabled
-               (0 << UDRIE0) | (0 << TXCIE0) | (0 << RXCIE0)); // ints off.
+  *_ucsrb &= ~((1 << TXEN) | (1 << RXEN) | // 9bits, enabled
+               (0 << UDRIE) | (0 << TXCIE) | (0 << RXCIE)); // ints off.
 }
 
 #define SERIAL9_ESCAPE (0xff)
@@ -118,15 +129,15 @@ void Serial9::loop(void)
   // host using the USB9_ESCAPE sequence if necessary.
 
   uint8_t status = *_ucsra;
-  
+
   // A character has been received by the UART
 
-  if (status & (1 << RXC0)) {
+  if (status & (1 << RXC)) {
 
     // Check if the 9th bit is set and send the possibly ESCAPED data
     // back to the host
 
-    bool bit9_hi = (0 != (*_ucsrb & (1 << RXB80)));
+    bool bit9_hi = (0 != (*_ucsrb & (1 << RXB8)));
     uint8_t data = *_udr;
 
     if (bit9_hi) {
@@ -139,11 +150,11 @@ void Serial9::loop(void)
     } else {
       Serial.write(data);
     }
-  
+
   // The UART is NOT ready to send a character, do nothing
 
-  } else if (0 == (status & (1 << UDRE0))) {
-    
+  } else if (0 == (status & (1 << UDRE))) {
+
   // The UART is ready to send a character, is there Serial data?
 
   } else if (Serial.available() > 0) {
@@ -155,7 +166,7 @@ void Serial9::loop(void)
       // Do nothing
     } else {
         _written = true;
-        *_ucsra |= (1 << TXC0); // Write a 1 to clear the TXC0 bit
+        *_ucsra |= (1 << TXC); // Write a 1 to clear the TXC0 bit
         digitalWrite(DE, HIGH);
     }
 
@@ -168,7 +179,7 @@ void Serial9::loop(void)
 
       } else {
         // It's a regular character, just send it
-        *_ucsrb &= ~(1 << TXB80);
+        *_ucsrb &= ~(1 << TXB8);
         *_udr = b;
       }
       break;
@@ -181,7 +192,7 @@ void Serial9::loop(void)
       } else if (SERIAL9_ESCAPE == b) {
         // It's an escaped ESCAPE character, just send it
         rx_state = SERIAL9_STATE_IDLE;
-        *_ucsrb &= ~(1 << TXB80);
+        *_ucsrb &= ~(1 << TXB8);
         *_udr = b;
 
       } else {
@@ -193,15 +204,15 @@ void Serial9::loop(void)
      case SERIAL9_STATE_HIGH:
        // It's a character that should be sent with the 9th bit high
        rx_state = SERIAL9_STATE_IDLE;
-       *_ucsrb |= (1 << TXB80);
+       *_ucsrb |= (1 << TXB8);
        *_udr = b;
-       break;       
+       break;
      }
 
   // The UART has completed the current character and there are no
   // incoming charaters availablke from the USB - force DE low
 
-  } else if (0 != (status & (1 << TXC0))) {
+  } else if (0 != (status & (1 << TXC))) {
 
     // Force DE high, we will be writing a character soon
     if (_written) {
